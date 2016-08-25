db72aff6ac1aa73dcfe4b72c92e5b2e1e574a1ba
Xingjing Lu <xingjinglu@gmail.com>
2016-08-25 10:28:55 +0800

    The right version of laplacian under /parac, with input as: 256, 512, 1024, 2048, 4096.

--- laplacian/parac/laplacian_opt_lxj_2048.c
+++ laplacian/parac/laplacian_opt_lxj_2048.c
@@ -711,7 +711,7 @@ int Laplacian(int Height, int Width, unsigned char *Src1, unsigned char *layer1)
 
 
 #ifdef _VERIFY
-    for(int i = 0; i < 1; i++){
+    for(int i = 1; i < 1; i++){
       for(int j = 0; j < 16; j++){
         //if(filter_cpu[i*1024+j] !=((unsigned char*) dst_horizon)[i* 1024 +j])
         {
